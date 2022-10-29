7a2c94efc6119da22edd51ee178014a688682fbb
Safal <safaldhungana7@gmail.com>
2022-10-29 12:47:42 +0545

    first-commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed.jsx';
+import LoginForm from './components/LoginForm.jsx';
+import './App.css';
+
+const projectID = 'ec50e0d0-f227-4bf6-b158-6abe716ec7b9';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
